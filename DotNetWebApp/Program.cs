cf5bd482d2440f4d09f6830f069aa18822d793c3
SzilvasiPeter <peti.szilvasi95@gmail.com>
2019-03-20 12:22:58 +0100

    Change the layout title Seed data when no item in the database

--- DotNetWebApp/Program.cs
+++ DotNetWebApp/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using DotNetWebApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DotNetWebApp
 {
@@ -14,12 +17,31 @@ namespace DotNetWebApp
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<MovieContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+                }
+                catch(Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseSetting("https_port", "8080")
+                // .UseSetting("https_port", "8080")
                 .UseStartup<Startup>();
     }
 }
