b2341a07527d4e1043b3b77a51cd01118da83d4b
SzilvasiPeter <peti.szilvasi95@gmail.com>
2019-03-25 20:12:30 +0100

    About page is done

--- DotNetWebApp/wwwroot/js/site.js
+++ DotNetWebApp/wwwroot/js/site.js
@@ -3,8 +3,20 @@
 
 // Write your Javascript code.
 
-// Faster carousel
-$('.carousel').carousel({
-    interval: 3000,
-    cycle: true
-})
+  
+    // Scrolling navigation bar
+    $(window).scroll(function() {
+      if($(window).scrollTop() > 80){
+        $('.myLogo').css({'height': '70px', 'width' : '70px'});
+        $('.nav-link').css({'margin' : '8px'});
+       }else{
+        $('.myLogo').css({'height': '100px', 'width' : '100px'});
+        $('.nav-link').css({'margin-left' : '40px', 'margin-right' : '40px'});
+       }
+     })
+
+    // Faster carousel
+    $('.carousel').carousel({
+        interval: 2500,
+        cycle: true
+    })
